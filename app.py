3cfe6bff3a2e8d4f13ccc77ceb3a6476332dc66d
dp <dpan7@ucsc.edu>
2022-02-25 22:52:59 -0800

    working on bridge

--- app.py
+++ app.py
@@ -2,59 +2,58 @@ import os
 from pymongo import MongoClient
 from pprint import pprint
 from dotenv import load_dotenv
+from flask import Flask
+from flask import render_template, url_for, flash, redirect, request, abort
+app = Flask(__name__)
+#from flaskblog import app, db, bcrypt
 load_dotenv()
 cluster = MongoClient(os.getenv("CLUSTER"))
 db = cluster["Filtered_Articles"]
+import json
 import requests
 from bs4 import BeautifulSoup as soup
 import newspaper
 from newspaper import Article
 import re
 
-#Websites = ["https://www.latimes.com","https://www.cbsnews.com","https://www.nbcnews.com","https://www.cnn.com","https://www.nytimes.com","https://www.latimes.com","https://www.wsj.com"]
 
 keywords_to_check = ["Ukraine","ukraine", "Putin","putin", "War","war", "Biden", "biden", "Russia","russia", "Belarus","belarus", "WW3", "bombing"]
-#links = []
+
 DATA = {0: {
             "title":'0',
             "url":'0',
-            "authors":'0',
             "publish_date":'0'
             },
         }
+#my_url = "https://www.latimes.com"
+#sections = ["/world-nation","/politics"]
 
 my_url = "https://www.nytimes.com"
 sections = ["/section/world","/section/politics"]
-#my_url = "https://www.nytimes.com/section/politics"
 
-def get_content_string(url):
+def get_data(url):
     page = requests.get(url)
     page_soup = soup(page.content, 'html.parser')
-    # Use the below statement as a visualizer of the HTML outline.
-    #print(page_soup)
     containers = page_soup.find_all("script", {"type": "application/ld+json"})
-
     article_list = []
     for container in containers:
         for dictionary in container:
             article_list.append(dictionary)
-
     article_list[0:2] = [''.join(article_list[0:2])]
-    content_string = article_list[0]
-    article_index = content_string.index("itemListElement")
-    content_string = content_string[article_index + 18:]
-
-    return content_string
-
+    content = article_list[0]
+    article_index = content.index("itemListElement")
+    content = content[article_index + 18:]
+    return content
 
 # Returns a list of URLS
-def urlfilter(content_string):
+def urlfilter(content):
     Filtered=[]
-    for match in re.findall('url":"(.*?)"."pos', content_string, re.S):
+    for match in re.findall('url":"(.*?)"."pos', content, re.S):
         if any(ext in match for ext in keywords_to_check): # check keyword
             if filter_title(match)!= False:
                 Filtered.append(match)
     return Filtered
+
 # Returns a filtered title
 def filter_title(title):
     #print (type(title))
@@ -89,43 +88,42 @@ def get_summary(link):
     content = soupy.find('meta', {'name':'description'}).get('content')
     return content
 
-for subcat in sections:
-    #print(my_url+subcat)
-    temp = get_content_string(my_url+subcat)
-    urls = urlfilter(temp)
-    #for i in urls:
+def browse():
+    for subcat in sections:
+        temp = get_data(my_url+subcat)
+        urls = urlfilter(temp)
+        collection = db[my_url]
+        for ind,link in enumerate(urls):
+            #print(link)
+            print(get_summary(link))
+            #print(get_title(link))
+            #print(get_date(link))
+            DATA[ind]= {}
+            DATA[ind]["title"] = get_title(link)
+            DATA[ind]["url"] = link
+            DATA[ind]["publish_date"] = get_date(link)
+            DATA[ind]["summary"] = get_summary(link)
+            collection.insert_one(DATA[ind])
+
+
+
+# This call will give you new data
+#@app.route(‘/refreshdata’)
+#def refreshdata():
+    #return ’This page could be used to present plain text or a form for user input!’
+
+@app.route("/getdata")
+def getdata():
+    mycollection = db["https://www.nytimes.com"]
+    print(db)
+    #all_hist =
+
+    x = mycollection.find({})
+    y = json.dumps(x)
+    return y;
+    for i in x:
         #print(i)
-    collection = db[my_url]
-    for ind,link in enumerate(urls):
-        print(link)
-        print(get_title(link))
-        #print(link)
-        print(get_date(link))
-
-        DATA[ind]= {}
-        DATA[ind]["title"] = get_title(link)
-        DATA[ind]["url"] = link
-        #print(get_date(link))
-        DATA[ind]["publish_date"] = get_date(link)
-        collection.insert_one(DATA[ind])
+        print(i["title"])
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#print(links[0])
-
-# Issue the serverStatus command and print the results
-
-
-#post = {"_id": 1, "name": "Lue", "url": "idk"}
+getdata()
